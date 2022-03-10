d3487d617537704605043830220600273e738e14
Stephen Beitzel <sbeitzel@pobox.com>
2022-03-10 15:00:08 -0800

    removing useless tests

--- Package.swift
+++ Package.swift
@@ -13,16 +13,11 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.2")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "Mirror-Package",
             dependencies: [
                 .product(name: "SystemPackage", package: "swift-system"),
                 .product(name: "ArgumentParser", package: "swift-argument-parser")
             ]),
-        .testTarget(
-            name: "Mirror-PackageTests",
-            dependencies: ["Mirror-Package"]),
     ]
 )
