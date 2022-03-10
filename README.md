12eb826315baf057cd8d238483bcb2eedf26ae4f
Stephen Beitzel <sbeitzel@pobox.com>
2022-03-09 21:33:56 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Mirror-Package
+
+A command-line tool to create or update a local mirror of the dependencies for a
+Swift project.
---
+++ Sources/Mirror-Package/Mirror.swift
@@ -0,0 +1,10 @@
+import ArgumentParser
+import Foundation
+
+@main
+struct Mirror: ParsableCommand {
+
+    func run() {
+        print("Creating dependency mirror for package!")
+    }
+}
